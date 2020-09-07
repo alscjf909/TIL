b2740b6e3fe3d17a3182966ffdf9947de7076f9b
alscjf909 <alscjf909@naver.com>
2020-09-08 01:45:48 +0900

    200908

--- Datastructure/HW_1/HW_1.c
+++ Datastructure/HW_1/HW_1.c
@@ -1,17 +1,76 @@
-#include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS   
-int main(void) {
-  int s1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int s2[10];
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
 
-  for(int i=0; i<10;i++){
-    printf("%d ,", s1[i]);
+int main() {
+    
+  char str1[80];
+  char str2[80];
+  int num_str1[80];
+  int num_str2[80];
+  const char s[5] = ",";
+  char *token1;
+
+
+  int answer[20];
+
+  //INPUT
+  printf("please insert 10 integer number series in ascending order twice\n");
+  scanf(" %[^\n]", str1);
+  scanf(" %[^\n]", str2);
+
+
+  //converts a string to an integer
+  //divide word on "," 
+  token1 = strtok(str1, s);
+
+  int i,j,k = 0;
+  while(token1 != NULL){
+
+    token1 = strtok(NULL, s);
+
+    i++;
   }
-  printf("\n");
-  scanf("%d, %d, %d, %d, %d, %d, %d, %d,%d, %d",&s1[0], &s1[1], &s1[2],&s1[3], &s1[4],&s1[5], &s1[6],&s1[7], &s1[8],&s1[9]);
-  
-  for(int i=0; i<10;i++){
-    printf("%d ,", s1[i]);
+    printf("Input1 : %s\n",str1);
+  printf("Input2 : %s\n",str2);/*
+  i=0;
+  while(num_str1[i] != '\0' && num_str2[j] != '\0'){
+    if(num_str1[i]>num_str2[j]){
+      answer[k]=num_str2[j];
+      printf("%d",answer[k]);
+      k++;
+      j++;
+    }
+    else if(num_str1[i]<=num_str2[j]){
+      answer[k]=num_str1[i];
+      printf("%d",answer[k]);
+      k++;
+      i++;
+    }
+ for (i = 0; i < sizeof(answer) / sizeof(int); i++)    // 배열의 요소 개수만큼 반복
+    {
+        printf("%d\n", answer[i]);    // 배열의 인덱스에 반복문의 변수 i를 지정
+    }
   }
+  for(k=0;k<20;k++){
+    printf("%d",answer[k]);
+  }
+  k=0;
+  while(answer[k]=='\0'){
+    k++;
+    if(answer[k]<answer[k-1]){
+      printf("The input numbers are not in ascending order");
+      return 0;
+    }
+    if(i+j != 20){
+      printf("You should input 20 numbers in total");
+      return 0;
+    }
+
+  }*/
+
+
+
   return 0;
+
 }

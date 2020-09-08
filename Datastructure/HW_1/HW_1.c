fc0c7673c5261b4dcc61cd561a3d6134bd2e4878
alscjf909 <alscjf909@naver.com>
2020-09-08 16:12:26 +0900

    200908

--- Datastructure/HW_1/HW_1.c
+++ Datastructure/HW_1/HW_1.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include<string.h>
 #include<limits.h>
 
 int main() {
     
-  char str1[80];
-  char str2[80];
-  int num_str1[80];
-  int num_str2[80];
+  char str1[80];  //insert string1
+  char str2[80];  //insert string2
+  int num_str1[80]; //
+  int num_str2[80]; //
   const char s[5] = ",";
   char *token1;
   char *token2;
@@ -25,71 +25,76 @@ int main() {
 
   //converts a string to an integer
   //divide word on ","
-  int i=0;
-  int j=0;
-  int k=0; 
+  int index1=0;
+  int index2=0;
+
   token1 = strtok(str1, s);
   while(token1 != NULL){
-    num_str1[i]=atoi(token1);
+    num_str1[index1]=atoi(token1);
     token1 = strtok(NULL, s);
-    i++;
+    index1++;
   }
   token2 = strtok(str2, s);
   while(token2 != NULL){
-    num_str2[j]=atoi(token2);
+    num_str2[index2]=atoi(token2);
     token2 = strtok(NULL, s);
-    j++;
+    index2++;
   }
-  //ERROR 
-  if((i+j) != 20){
+
+
+  //ERROR We must inser total 20 numbers
+  if((index1+index2) != 20){
     printf("You should input 20 numbers in total");
     return 0;
   }
 
-
   //Set Dummy
-  num_str1[i]=INT_MAX;
-  num_str2[j]=INT_MAX;
+  num_str1[index1]=INT_MAX;
+  num_str2[index2]=INT_MAX;
 
   //reset
-  i=0;
-  j=0;
+	index1=0;
+  index2=0;
+  int num=0; // Overlaped Number count
 
-
+  int index3=-1; 
   //sort alogorithm
-  while(!(num_str1[i]==INT_MAX && num_str2[j]==INT_MAX)){
-    if(num_str1[i]>num_str2[j]){
-      answer[k]=num_str2[j];
-      k++;
-      j++;
+  while(!(num_str1[index1]==INT_MAX && num_str2[index2]==INT_MAX)){
+    if(num_str1[index1]>num_str2[index2]){
+      index3++;
+      answer[index3]=num_str2[index2];
+      index2++;
     }
-    else if(num_str1[i]<num_str2[j]){
-      answer[k]=num_str1[i];
-      k++;
-      i++;
+    else if(num_str1[index1]<num_str2[index2]){
+      index3++;
+      answer[index3]=num_str1[index1];      
+      index1++;
     }
-    else if(num_str1[i]==num_str2[j]){
-      i++;
+    else if(num_str1[index1]==num_str2[index2]){
+      index3++;
+      answer[index3]=num_str1[index1];
+      index1++;
+      index2++;		
+      num++;
     }
     
   }
 
   //ERROR
-  for(i=1;i<sizeof(answer)/sizeof(int)-1;i++){
+  
+  for(int i=1;i<sizeof(answer)/sizeof(int)-num-1;i++){
     if(answer[i]<answer[i-1]){
       printf("The input numbers are not in ascending order\n");
-      printf("i : %d",i);
+      printf("num : %d",num);
       return 0;
+      
     }
   }
-    for(i=0;i<sizeof(answer)/sizeof(int);i++){
+  printf("Output : ");
+    for(int i=0;i<sizeof(answer)/sizeof(int);i++){
     if(answer[i] != INT_MAX && answer[i] != '\0')
-      printf("%d\n", answer[i]);
+      printf("%d, ", answer[i]);
     }
-    
-
-
-
 
   return 0;
 

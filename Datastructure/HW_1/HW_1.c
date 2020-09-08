e42892514a20c3536f463665d19fcf7fd3156171
alscjf909 <alscjf909@naver.com>
2020-09-08 14:51:25 +0900

    200908

--- Datastructure/HW_1/HW_1.c
+++ Datastructure/HW_1/HW_1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<string.h>
+#include<limits.h>
 
 int main() {
     
@@ -10,64 +11,83 @@ int main() {
   int num_str2[80];
   const char s[5] = ",";
   char *token1;
+  char *token2;
 
 
-  int answer[20];
+  int answer[20]={INT_MAX,};
 
   //INPUT
   printf("please insert 10 integer number series in ascending order twice\n");
+  printf("Input1 : ");
   scanf(" %[^\n]", str1);
+  printf("Input2 : ");
   scanf(" %[^\n]", str2);
 
-
   //converts a string to an integer
-  //divide word on "," 
+  //divide word on ","
+  int i=0;
+  int j=0;
+  int k=0; 
   token1 = strtok(str1, s);
-
-  int i,j,k = 0;
   while(token1 != NULL){
-
+    num_str1[i]=atoi(token1);
     token1 = strtok(NULL, s);
-
     i++;
   }
-    printf("Input1 : %s\n",str1);
-  printf("Input2 : %s\n",str2);/*
+  token2 = strtok(str2, s);
+  while(token2 != NULL){
+    num_str2[j]=atoi(token2);
+    token2 = strtok(NULL, s);
+    j++;
+  }
+  //ERROR 
+  if((i+j) != 20){
+    printf("You should input 20 numbers in total");
+    return 0;
+  }
+
+
+  //Set Dummy
+  num_str1[i]=INT_MAX;
+  num_str2[j]=INT_MAX;
+
+  //reset
   i=0;
-  while(num_str1[i] != '\0' && num_str2[j] != '\0'){
+  j=0;
+
+
+  //sort alogorithm
+  while(!(num_str1[i]==INT_MAX && num_str2[j]==INT_MAX)){
     if(num_str1[i]>num_str2[j]){
       answer[k]=num_str2[j];
-      printf("%d",answer[k]);
       k++;
       j++;
     }
-    else if(num_str1[i]<=num_str2[j]){
+    else if(num_str1[i]<num_str2[j]){
       answer[k]=num_str1[i];
-      printf("%d",answer[k]);
       k++;
       i++;
     }
- for (i = 0; i < sizeof(answer) / sizeof(int); i++)    // 배열의 요소 개수만큼 반복
-    {
-        printf("%d\n", answer[i]);    // 배열의 인덱스에 반복문의 변수 i를 지정
+    else if(num_str1[i]==num_str2[j]){
+      i++;
     }
+    
   }
-  for(k=0;k<20;k++){
-    printf("%d",answer[k]);
-  }
-  k=0;
-  while(answer[k]=='\0'){
-    k++;
-    if(answer[k]<answer[k-1]){
-      printf("The input numbers are not in ascending order");
-      return 0;
-    }
-    if(i+j != 20){
-      printf("You should input 20 numbers in total");
-      return 0;
-    }
 
-  }*/
+  //ERROR
+  for(i=1;i<sizeof(answer)/sizeof(int)-1;i++){
+    if(answer[i]<answer[i-1]){
+      printf("The input numbers are not in ascending order\n");
+      printf("i : %d",i);
+      return 0;
+    }
+  }
+    for(i=0;i<sizeof(answer)/sizeof(int);i++){
+    if(answer[i] != INT_MAX && answer[i] != '\0')
+      printf("%d\n", answer[i]);
+    }
+    
+
 
 
 
